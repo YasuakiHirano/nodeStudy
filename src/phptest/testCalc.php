a2593b470a8848ff68616d16355c8d71d6d9a9d9
YasuakiHirano <yasuakihappy@gmail.com>
2016-08-24 00:10:26 +0900

    comment&calc add

---
+++ src/phptest/testCalc.php
@@ -0,0 +1,10 @@
+<?php
+    $test1 = 10 + 3; //足し算
+    $test2 = 10 - 7; //引き算
+    $test3 = 2 * 7; //掛け算
+    $test4 = 8 / 2; //割り算
+    
+    echo "$test1\n";
+    echo "$test2\n";
+    echo "$test3\n";
+    echo "$test4\n";
---
+++ src/phptest/testCalc2.php
@@ -0,0 +1,15 @@
+<?php
+    $test1 = 10;
+    $test2 = 11;
+    $test3 = 12;
+    $test4 = 13;
+
+    $test1 = $test1++;
+    $test2 = ++$test2;
+    $test3 = $test3--;
+    $test4 = --$test4;
+
+    echo "$test1\n";
+    echo "$test2\n";
+    echo "$test3\n";
+    echo "$test4\n";
